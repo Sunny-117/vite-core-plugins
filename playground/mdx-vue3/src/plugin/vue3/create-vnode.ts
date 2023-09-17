8f6f64fbe365f1aa9f995473278371ea2b6e3eab
fuzhiqiang <fuzhiqiang@baidu.com>
2023-09-17 18:31:06 +0800

    feat: create-vnode

---
+++ playground/mdx-vue3/src/plugin/vue3/create-vnode.ts
@@ -0,0 +1,79 @@
+import {
+  Fragment,
+  type PropType,
+  type VNodeTypes,
+  createVNode,
+  defineComponent,
+} from 'vue'
+import { type PropsComponents, useMDXComponents } from './context'
+
+const TYPE_PROP_NAME = 'mdxType'
+
+const DEFAULTS = {
+  inlineCode: 'code',
+  wrapper: (props: any, { slots }: any) => createVNode(Fragment, {}, slots),
+}
+const MDXCreateComponent = defineComponent({
+  name: 'MDXCreateComponent',
+  props: {
+    components: {
+      type: Object as PropType<PropsComponents>,
+      default: () => ({}),
+    },
+    originalType: String,
+    mdxType: String,
+    parentName: String,
+  },
+  setup(props, { slots }) {
+    const componentsRef = useMDXComponents(() => props.components)
+
+    return () => {
+      const components: any = componentsRef.value
+      const { parentName, originalType, mdxType: type, ...etc } = props
+      const Component =
+        components[`${parentName}.${type}`] ||
+        components[type!] ||
+        DEFAULTS[type] ||
+        originalType
+      return createVNode(
+        Component,
+        {
+          ...etc,
+        },
+        slots
+      )
+    }
+  },
+})
+export function mdx(
+  type: VNodeTypes,
+  props: any,
+  children: unknown,
+  patchFlag?: number,
+  dynamicProps?: string[] | null,
+  isBlockNode?: boolean
+) {
+  let component = type
+  let newProps = props
+  const mdxType = props && props.mdxType
+  if (typeof type === 'string' || mdxType) {
+    component = MDXCreateComponent
+    newProps = {}
+    // eslint-disable-next-line no-restricted-syntax
+    for (const key in props) {
+      if (Object.prototype.hasOwnProperty.call(props, key)) {
+        newProps[key] = props[key]
+      }
+    }
+    newProps.originalType = type
+    newProps[TYPE_PROP_NAME] = typeof type === 'string' ? type : mdxType
+  }
+  return createVNode(
+    component,
+    newProps,
+    children,
+    patchFlag,
+    dynamicProps,
+    isBlockNode
+  )
+}
--- playground/mdx-vue3/vite.config.ts
+++ playground/mdx-vue3/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 // import pluginMdx from 'vite-plugin-mdx'
-import pluginMdx from './plugin'
+import pluginMdx from './src/plugin/plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
