41685eba3e08803bb7be9c2e1c3e714f7c0dd6c0
Sunny-117 <zhiqiangfu6@gmail.com>
2024-09-29 16:35:41 +0800

    feat: virtual module plugin

--- packages/foo/src/index.ts
+++ packages/foo/src/index.ts
@@ -1,3 +1,22 @@
-export const foo = 'foo'
+import { Plugin } from 'vite'
 
-export default () => {}
+interface Options {}
+export const virtualModuleSignal = 'virtual-module'
+
+export default (options: Options): Plugin => {
+  return {
+    name: 'foo',
+    resolveId(source) {
+      if (source === virtualModuleSignal) {
+        return source
+      }
+      return null
+    },
+    load(id) {
+      if (id === virtualModuleSignal) {
+        return `export default 'This is virtual!'`
+      }
+      return null
+    },
+  }
+}
