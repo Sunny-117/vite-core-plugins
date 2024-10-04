77d21d0557829578243eebb0c2403d36182c1150
Sunny-117 <zhiqiangfu6@gmail.com>
2024-10-04 17:58:31 +0800

    prerender

---
+++ playground/ssr/prerender.ts
@@ -0,0 +1,20 @@
+// build的时候
+// 静态站生成器
+// ssr实现静态站点导出
+import fs from 'node:fs'
+const template = fs.readFileSync('dist/client/index.html', 'utf-8')
+import { render } from './dist/server/server-entry'
+
+const routesToRender = fs
+  .readdirSync('src/pages')
+  .map((file) => file.replace('.jsx', '').toLowerCase())
+
+for (const route of routesToRender) {
+  const context = {}
+  const html = render(route, context)
+  const responseHtml = template.replace('<!-- APP_HEML -->', html)
+  const filePath = `dist/static/${route}.html`
+  fs.writeFile(filePath, responseHtml, 'utf-8', () => {
+    console.log('success')
+  })
+}
